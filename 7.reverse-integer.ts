c966acbac35548f54561881358e9513769cbe58a
Anais.yun <38196875+dev-anais@users.noreply.github.com>
2025-02-09 22:24:30 +0900

    speed up 7

--- 7.reverse-integer.ts
+++ 7.reverse-integer.ts
@@ -1,8 +1,15 @@
 function reverse(x: number): number {
-  const absoluteNumber = Math.abs(x);
-  const multiplyForAbs = absoluteNumber !== x ? -1 : 1;
-  return Number(absoluteNumber.toString().split('').reverse().join('')) * multiplyForAbs;
+  const isNegative = x < 0;
+  let result = 0;
+  x = Math.abs(x);
+  while (x > 0) {
+    const remain = x % 10;
+    result = result * 10 + remain;
+    x = Math.floor(x / 10);
+  }
+  console.log(result);
+  return isNegative ? result * -1 : result;
 }
 
-console.log(reverse(103));
-console.log(reverse(100));
+console.log(reverse(-103));
+console.log(reverse(401));
